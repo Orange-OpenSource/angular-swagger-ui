9162c920b7c3711dc1033afca6f1cb54eb9e7533
Mathieu Ales <mathieu.ales@orange.com>
2015-03-10 11:41:17 +0100

    add comments

--- src/scripts/swagger-ui-templates.js
+++ src/scripts/swagger-ui-templates.js
@@ -1,4 +1,10 @@
-/**
- * used to create a module containing templates at build phase
+/*
+ * angular-swagger-ui
+ * http://github.com/maales/angular-swagger-ui
+ * Version: 0.1.0 - 2015-02-26
+ * License: MIT
  */
+
+// this module will be created at build phase and will contain swagger-ui HTML template
+// this file won't be part of the built JS file
 angular.module('swaggerUiTemplates', []);
