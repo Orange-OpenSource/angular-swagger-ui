ee818226d29d057256064bbc41bac57446e9b55a
Mathieu Ales <mathieu.ales@orange.com>
2015-08-12 09:18:30 +0200

    update gruntfile to display help

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,19 @@ module.exports = function(grunt) {
             pkg: grunt.file.readJSON('package.json')
         };
 
+    grunt.log.writeln(' ');
+    grunt.log.writeln('********************** command help **********************');
+    grunt.log.writeln(' ');
+    grunt.log.writeln(' $ grunt $0:$1');
+    grunt.log.writeln('');
+    grunt.log.writeln(' $0');
+    grunt.log.writeln('     dev     develop swagger-ui');
+    grunt.log.writeln('     prod    build swagger-ui');
+    grunt.log.writeln('');
+    grunt.log.writeln(' $1 (optional) for dev & prod');
+    grunt.log.writeln('     server          webserver on 127.0.0.1');
+    grunt.log.writeln('');
+
     /**
      * @description
      * Concatenate all .js files in path to a single object.
