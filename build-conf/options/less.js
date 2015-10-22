be1944f521b676d61a8651a1df47dd126af58ffb
Mathieu Ales <mathieu.ales@orange.com>
2015-10-22 14:43:28 +0200

    use angular and bootstrap from bower in dev

--- build-conf/options/less.js
+++ build-conf/options/less.js
@@ -1,7 +1,8 @@
 module.exports = {
 	dev: {
 		files: {
-			'src/css/swagger-ui.css': 'src/less/main.less'
+			'src/css/swagger-ui.css': 'src/less/main.less',
+			'src/css/bootstrap.css': 'bower_components/bootstrap-less-only/less/bootstrap.less'
 		}
 	},
 	distcompressed: {
