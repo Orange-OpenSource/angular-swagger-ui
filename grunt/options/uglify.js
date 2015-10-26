f3851e281a4bfd951e4c6dc1d714cad28b3f1c14
Mathieu Ales <mathieu.ales@orange.com>
2015-10-26 17:26:27 +0100

    remove comments

--- grunt/options/uglify.js
+++ grunt/options/uglify.js
@@ -3,21 +3,11 @@
 //
 module.exports = {
 	dist: {
-		/*src: ['dist/scripts/swagger-ui.js'],
-		dest: 'dist/scripts/swagger-ui.min.js'*/
 		files: {
 			'dist/scripts/swagger-ui.min.js': ['dist/scripts/swagger-ui.js'],
 			'dist/scripts/modules/swagger-external-references.min.js': ['dist/scripts/modules/swagger-external-references.js'],
 			'dist/scripts/modules/swagger-xml-formatter.min.js': ['dist/scripts/modules/swagger-xml-formatter.js'],
 			'dist/scripts/modules/swagger1-to-swagger2-converter.min.js': ['dist/scripts/modules/swagger1-to-swagger2-converter.js']
 		}
-	}/*,
-	distExternals: {
-		src: ['dist/scripts/modules/swagger-external-references.js'],
-		dest: 'dist/scripts/modules/swagger-external-references.min.js'
-	},
-	distXml: {
-		src: ['dist/scripts/modules/swagger-xml-formatter.js'],
-		dest: 'dist/scripts/modules/swagger-xml-formatter.min.js'
-	}*/
+	}
 }
