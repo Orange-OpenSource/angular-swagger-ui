852527b247d85b0a71ce3193babe7a8fd5ec8bb8
Mathieu Ales <mathieu.ales@orange.com>
2016-03-17 14:47:46 +0100

    format file

--- grunt/options/watch.js
+++ grunt/options/watch.js
@@ -2,22 +2,22 @@
 // Watches files for changes and runs tasks based on the changed files
 //
 module.exports = {
-  less: {
-    files: ['src/less/{,*/}*.less'],
-    tasks: ['less:dev']
-  },
-  js: {
-    files: ['src/scripts/{,*/}*.js'],
-    tasks: ['newer:jshint:all'],
-  },
-  livereload: {
-    options: {
-      livereload: '<%= connect.options.livereload %>'
+    less: {
+        files: ['src/less/{,*/}*.less'],
+        tasks: ['less:dev']
     },
-    files: [
-      'src/{,*/}*.html',
-      'src/css/{,*/}*.css',
-      'src/scripts/{,*/}*.js'
-    ]
-  }
+    js: {
+        files: ['src/scripts/{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+    },
+    livereload: {
+        options: {
+            livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+            'src/{,*/}*.html',
+            'src/css/{,*/}*.css',
+            'src/scripts/{,*/}*.js'
+        ]
+    }
 }
