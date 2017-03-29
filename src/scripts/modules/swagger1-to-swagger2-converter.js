be1bd8ed9d7487118f181dc43c0b1205e0085c7c
Mathieu Ales <mathieu.ales@orange.com>
2017-03-29 16:39:32 +0200

    add support for map/dictionary in swagger 1

--- src/scripts/modules/swagger1-to-swagger2-converter.js
+++ src/scripts/modules/swagger1-to-swagger2-converter.js
@@ -137,7 +137,7 @@ angular
 				var ref = param.type || param.$ref;
 				if (swagger1.models && ref && swagger1.models[ref]) {
 					param.schema = {
-						$ref: '#/definitions/' + ref
+						$ref: getModelReference(ref)
 					};
 					delete param.type;
 				}
@@ -153,7 +153,7 @@ angular
 				if (resp.responseModel) {
 					if (swagger1.models && swagger1.models[resp.responseModel]) {
 						response.schema = {
-							$ref: '#/definitions/' + resp.responseModel
+							$ref: getModelReference(resp.responseModel)
 						};
 					} else {
 						response.type = resp.responseModel;
@@ -161,15 +161,15 @@ angular
 				} else if (resp.code === 200 && operation.type !== 'void') {
 					response.schema = {
 						type: operation.type,
-						$ref: '#/definitions/' + operation.type
+						$ref: getModelReference(operation.type)
 					};
 					if (operation.type === 'array') {
 						delete response.schema.$ref;
 						var ref = operation.items.type || operation.items.$ref,
 							items = response.schema.items = {};
-							
+
 						if (swagger1.models && swagger1.models[ref]) {
-							items.$ref = '#/definitions/' + ref;
+							items.$ref = getModelReference(ref);
 						} else {
 							items.type = ref;
 						}
@@ -179,7 +179,10 @@ angular
 		}
 
 		function convertModels(swagger1, swagger2) {
-			var subModel;
+			var subModel,
+				mapRegExp = new RegExp('Map\\[string,(.*)\\]'),
+				arrayRegExp = new RegExp('List\\[(.*)\\]');
+
 			angular.forEach(swagger1.models, function(model, name) {
 				swagger2.definitions[name] = model;
 				if (model.subTypes) {
@@ -196,23 +199,57 @@ angular
 				}
 				angular.forEach(model.properties, function(prop) {
 					var ref = prop.type || prop.$ref;
-					if (swagger1.models && ref && swagger1.models[ref]) {
-						prop.$ref = '#/definitions/' + ref;
+					if (ref && hasModelReference(ref, swagger1)) {
+						prop.$ref = getModelReference(ref);
 						delete prop.type;
-					}
-					if (prop.items) {
+					} else if (ref && ref.match(mapRegExp)) {
+						// this a map/dictionary
+						// natively not supported in Swagger 1.2, this fixes Java implementation
+						delete prop.$ref;
+						var subtype = ref.match(mapRegExp)[1];
+						if (subtype && subtype.match(arrayRegExp)) {
+							subtype = subtype.match(arrayRegExp)[1];
+							prop.type = 'object';
+							prop.additionalProperties = {
+								type: 'array'
+							};
+							prop.additionalProperties.items = generateProperty(subtype, swagger1);
+						} else {
+							prop.type = 'object';
+							prop.additionalProperties = generateProperty(subtype, swagger1);
+						}
+					} else if (prop.items) {
 						ref = prop.items.type || prop.items.$ref;
-						if (swagger1.models && ref && swagger1.models[ref]) {
-							prop.items.$ref = '#/definitions/' + ref;
+						if (hasModelReference(ref, swagger1)) {
+							prop.items.$ref = getModelReference(ref);
 							delete prop.items.type;
 						}
-					}
-					if (prop.enum) {
+					} else if (prop.enum) {
 						prop.type = 'string';
 						delete prop.$ref;
 					}
 				});
 			});
+		}
+
+		function generateProperty(type, swagger1) {
+			if (hasModelReference(type, swagger1)) {
+				return {
+					$ref: getModelReference(type)
+				};
+			} else {
+				return {
+					type: type
+				};
+			}
+		}
+
+		function hasModelReference(type, swagger1) {
+			return swagger1.models && swagger1.models[type];
+		}
+
+		function getModelReference(type) {
+			return '#/definitions/' + type;
 		}
 
 	})
