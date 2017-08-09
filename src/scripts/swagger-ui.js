907d6d1015896a545b48659490f91f9244f1871c
Mathieu Ales <mathieu.ales@orange.com>
2017-08-09 11:32:08 +0200

    select text on double click

--- src/scripts/swagger-ui.js
+++ src/scripts/swagger-ui.js
@@ -311,4 +311,20 @@ angular
 				});
 			}
 		};
+	})
+	.directive('selectOnDbClick', function($window) {
+		// helper to be able select all text on double click
+		return {
+			restrict: 'A',
+			link: function(scope, element) {
+				element.bind('dblclick', function() {
+					var selection = $window.getSelection(),
+						range = document.createRange();
+
+					range.selectNodeContents(element[0]);
+					selection.removeAllRanges();
+					selection.addRange(range);
+				});
+			}
+		};
 	});
